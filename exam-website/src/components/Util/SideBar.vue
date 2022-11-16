f77856205c0a44af66574525994632fdd1abd49c
mads <madsi2000@gmail.com>
2022-11-16 22:10:07 +0100

    Navn opdatering

--- exam-website/src/components/Util/SideBar.vue
+++ exam-website/src/components/Util/SideBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>
-            <p :class = "(MainPage == 'StartPage')?'btn-success':'btn-primary'" class="btn btn-primary rounded-0" @click="this.$emit('Update-Page', 'StartPage')">Start</p>
+            <p :class = "(MainPage == 'StartPage')?'btn-success':'btn-primary'" class="btn btn-primary rounded-0" @click="this.$emit('Update-Page', 'StartPage')">En action er kørt</p>
             <p :class = "(MainPage == 'ProduktgruppePage')?'btn-success':'btn-primary'" class="btn btn-primary rounded-0" v-if="this.firma_id != ''" @click="this.$emit('Update-Page', 'ProduktgruppePage')">Produktgrupper</p>
             <p :class = "(MainPage == 'EkspedientPage')?'btn-success':'btn-primary'" class="btn btn-primary rounded-0" v-if="this.firma_id != ''" @click="this.$emit('Update-Page', 'EkspedientPage')">Ekspedienter</p>
         </div>
